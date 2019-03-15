1a182763ee7b7a22e121e0f72f295e93e3431f52
Marunmin <r_ma3@fanshaweonline.ca>
2019-03-15 13:29:35 +0800

    login component js update

--- js/components/LoginComponent.js
+++ js/components/LoginComponent.js
@@ -1,29 +1,25 @@
 export default {
     template: `
-        <div class="container">
-            <div class="jumbotron roku-jumbotron">
-                <h1 class="display-4">Welcome to Flashback!</h1>
-                <p class="lead">Before revisiting your favourite movies, tv shows or music from yesteryear, please log in with a valid username and password.</p>
-                <hr class="my-4">
-                <form>
-                    <div class="form-row align-items-center">
-                        <div class="col-md-3 my-1">
-                            <label class="sr-only" for="inlineFormInputName">Name</label>
-                            <input v-model="input.username" type="text" class="form-control" id="inlineFormInputName" placeholder="username" required>
-                        </div>
-
-                        <div class="col-md-3 my-1">
-                            <label class="sr-only" for="inlineFormPassword">Name</label>
-                            <input v-model="input.password" type="password" class="form-control" id="inlineFormPassword" placeholder="password" required>
-                        </div>
-
-                        <div class="col-auto my-1">
-                            <button v-on:click.prevent="login()" type="submit" class="btn btn-primary">Go!</button>
-                        </div>
+            <div class="component-wrapper" id="login-form">
+                <div class="container">
+                    <div class="form-warp mx-auto">
+                        <h1>Sign in</h1>
+                        <p class="mt-3 mb-4">Before watching your favorite TV Shows on Roku, please log in with a valid username and password.</p>
+                        <form>
+                            <div class="form-group">
+                                <label class="sr-only" for="inlineFormInputName">Username</label>
+                                <input v-model="input.username" type="text" class="form-control" id="inlineFormInputName" placeholder="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label class="sr-only" for="exampleInputPassword1">Password</label>
+                                <input  v-model="input.password" type="password" class="form-control" id="inlineFormPassword" placeholder="password" required>
+                            </div>
+                            <button v-on:click.prevent="login()" type="submit" class="btn btn-primary">Submit</button
+            
+                        </form>
                     </div>
-                </form>            
+                </div>
             </div>
-        </div>
      `,
  
      data() {
