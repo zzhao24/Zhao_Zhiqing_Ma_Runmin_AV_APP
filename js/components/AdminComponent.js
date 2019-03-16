92d4e8a7883105eaba313b72516af28273cbf682
Marunmin <r_ma3@fanshaweonline.ca>
2019-03-16 10:02:11 +0800

    add admin container

--- js/components/AdminComponent.js
+++ js/components/AdminComponent.js
@@ -1,17 +1,22 @@
 export default {
+    props: ['currentuser'],
     template: `
-    <h1>Admin Dashboard</h1>
-    <h3>Welcome!</h3>
-    <p> {{ message }}</p>
-    <nav>
-        <ul>
-            <li><a href="#">Create User</a></li>
-            <li><a href="#">Edit User</a></li>
-            <li><a href="#">Delete User</a></li>
-            <li><a href="">Sign Out</a></li>
-            <!-- scripts/caller.php?caller_id=logout -->
-        </ul>
-    </nav>
+    <div class="component-wrapper" id="users">
+        <div class="container">
+            <h1>Admin Dashboard ???</h1>
+            <h3>Welcome! ???</h3>
+            <p> {{ message }}</p>
+            <nav>
+                <ul>
+                    <li><a href="#">Create User</a></li>
+                    <li><a href="#">Edit User</a></li>
+                    <li><a href="#">Delete User</a></li>
+                    <li><a href="">Sign Out</a></li>
+                    <!-- scripts/caller.php?caller_id=logout -->
+                </ul>
+            </nav>
+        </div>
+    </div>
     `,
 
     data() {
